5a8444bc092d7d0e032287b85e3a7f9cf1d2eb20
Ossi <K9447@student.jamk.fi>
2017-09-18 18:29:28 +0300

    Added kiuas, Pesukone, Tv

--- Labrat/Lab02.cs
+++ Labrat/Lab02.cs
@@ -13,43 +13,33 @@ namespace Labrat
             public int Virta { get; set; }
             public int Lampotila { get; set; }
             public int Kosteus { get; set; }
-            public Kiuas(int Lampotila, int Virta, int Kosteus) : base()
+
+            public void virta()
             {
-                this.Virta = Virta;
-                this.Lampotila = Lampotila;
-                this.Kosteus = Kosteus;
-            }
-            public Kiuas()
-            {
-            }
-        }
-        public class KiuasKaytto
-        {
-            public static void TestaaKiuas()
-            {
-                do
+                if (Virta == 1)
                 {
-                Kiuas OssinKiuas = new Kiuas();
-                Console.WriteLine("Pidetäänkö kiukaassa virta vai ei ?? 1 = päälle, Muu = poispäältä");
-                OssinKiuas.Virta = int.Parse(Console.ReadLine());
-                Console.Clear();
-                if (OssinKiuas.Virta == 1)
-                {
-                    Console.WriteLine("Mihin lämpötilaan haluat kiukaan nostaa ? >>");
-                    OssinKiuas.Lampotila = int.Parse(Console.ReadLine());
-                    Console.Clear();
-                    Console.WriteLine("Kiukaan teho on nyt " + OssinKiuas.Lampotila);
-                    Console.WriteLine("Anna kosteus prosentti. (0-100)");
-                    OssinKiuas.Kosteus = int.Parse(Console.ReadLine());
-                    Console.Clear();
-                    Console.WriteLine("Kiukaan kosteus on nyt " + OssinKiuas.Kosteus);
+                    Console.WriteLine("Kiuas on Päällä");
                 }
                 else
                 {
-                    Console.WriteLine("Kiuas on pois päältä");
-                        break;
+                    Console.WriteLine("kiuas ei ole Päällä");
                 }
-                } while (true);
+            }
+            public void lampotila()
+            {
+                Console.WriteLine("Lämpötila on {0} astetta", Lampotila);
+            }
+            public void kosteus()
+            {
+                if (Kosteus > 0)
+                {
+                    Lampotila += 5;
+                    Console.WriteLine("Heitit löylyä lämpötila nousi {0} asteeseen", Lampotila);
+                }
+                else
+                {
+                    Console.WriteLine("Lämpötila laski {0} asteeseen", Lampotila);
+                }
             }
         }
         public class Pesukone
@@ -59,62 +49,50 @@ namespace Labrat
             public int Aika { get; set; }
             public int Pesuaine { get; set; }
             public int Ohjelma { get; set; }
-            public Pesukone(int Virta, int Lampotila, int Aika, int Pesuaine) : base()
+
+            public void virta()
             {
-                this.Virta = 0;
-                this.Lampotila = Lampotila;
-                this.Aika = Aika;
-                this.Pesuaine = Pesuaine;
-                this.Ohjelma = Ohjelma;
-            }
-            public Pesukone()
-            {
-            }
-        }
-        public class PesukoneKaytto
-        {
-            public static void TestaaPesukone()
-            {
-                do
+                if (Virta == 1)
                 {
-                    Pesukone OssinPesukone = new Pesukone();
-                    Console.WriteLine("Pestäänko pyykkia ?? 1 = Pestään, 0 = ei pestä");
-                    OssinPesukone.Virta = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Pesukone on Päällä");
+                }
+                else
+                {
+                    Console.WriteLine("Pesukone ei ole Päällä");
+                }
+            }
+            public void lampotila()
+            {
+                Console.WriteLine("Lämpötila on {0} astetta", Lampotila);
+            }
+            public void aika()
+            {
+                if (Aika > 0)
+                {
+                    int tunnit = Aika / 60;
+                    int minuutit = Aika % 60;
+                    Console.WriteLine("Ohjelman kesto on {0} tuntia ja {1} minuuttia ", tunnit, minuutit);
+                }
+                else
+                {
+                    Console.WriteLine("Aikaa ei asetettu ohjelma sammuu");
                     Console.Clear();
-                    if (OssinPesukone.Virta == 1)
-                    {
-                        Console.WriteLine("Syötä lämpötila >>");
-                        OssinPesukone.Lampotila = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        Console.WriteLine("Pesukoneen lämpötila on asetettu " + OssinPesukone.Lampotila + " asteeseen");
-                        Console.WriteLine("Syötä pesuohjelman kesto >>");
-                        OssinPesukone.Aika = int.Parse(Console.ReadLine());
-                        int Tunnit = OssinPesukone.Aika / 60;
-                        int Minuutit = OssinPesukone.Aika % 60;
-                        Console.WriteLine("Syötä pesuaineen määrä (ml) >>");
-                        OssinPesukone.Pesuaine = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        Console.WriteLine("Pesukoneen lämpötila on asetettu " + OssinPesukone.Lampotila + " asteeseen");
-                        Console.WriteLine("Pesuohjelman Kesto on " + Tunnit + " tuntia ja " + Minuutit + " minuuttia ");
-                        Console.WriteLine("Pesuainetta syötetty " + OssinPesukone.Pesuaine + " ml");
-                        Console.WriteLine("Käynnistetäänkö pesuohjelma?(1/0) >> ");
-                        OssinPesukone.Ohjelma = int.Parse(Console.ReadLine());
-                        if (OssinPesukone.Ohjelma == 1)
-                        {
-                            Console.WriteLine("Pyykki pesty");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Pesuohjelma sammutettiin");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Pesukone pois päältä");
-                        break;
-                    }
-                } while (true);
+                }
+            }
+            public void pesuaine()
+            {
+                Console.WriteLine("Annoit pesuainetta {0} millilitraa",Pesuaine);
+            }
+            public void ohjelma()
+            {
+                if (Ohjelma == 1)
+                {
+                    Console.WriteLine("Pesuohjelma käynnistetty.");
+                }
+                else
+                {
+                    Console.WriteLine("Pesuohjelma Pysähtyi.");
+                }
             }
         }
         public class TV
@@ -122,19 +100,43 @@ namespace Labrat
             public int Virta { get; set; }
             public int Kanava { get; set; }
             public int Aanenvoimakkuus { get; set; }
-            public TV(int virta, int kanava, int aanenvoimakkuus) : base()
+
+            public void virta()
             {
-                this.Virta = virta;
-                this.Kanava = kanava;
-                this.Aanenvoimakkuus = aanenvoimakkuus;
+                if (Virta == 1)
+                {
+                    Console.WriteLine("TV on Päällä");
+                }
+                else
+                {
+                    Console.WriteLine("...........Epämääräistä siritystä");
+                }
             }
-            public TV()
+            public void kanava()
             {
+                Console.WriteLine("Kanava vaihtui ohjelmapaikalle {0}", Kanava);
+            }
+            public void aanenvoimakkuus()
+            {
+                if (Aanenvoimakkuus < 0)
+                {
+                    Console.WriteLine("ÄÄnenvoimakkuus pienenee {0} yksikköä", Aanenvoimakkuus);
+                }
+                if (Aanenvoimakkuus > 0)
+                {
+                    Console.WriteLine("ÄÄnenvoimakkuus nousee {0} yksikköä",Aanenvoimakkuus);
+                }
             }
         }
-        public class TVkaytto
+        public class Vehicle
         {
-            
+            public string Name { get; set; }
+            public int Speed { get; set; }
+            public int Tyres { get; set; }
+            public void name()
+            {
+                return Name;
+            }
         }
     }
 }
--- Labrat/Program.cs
+++ Labrat/Program.cs
@@ -10,7 +10,48 @@ namespace Labrat
     {
         static void Main(string[] args)
         {
-            Lab02.PesukoneKaytto.TestaaPesukone();
+            /*Lab02.Kiuas uus = new Lab02.Kiuas();
+
+            Console.WriteLine("Onko kiuas päällä (1/0)? >>");
+            uus.Virta = int.Parse(Console.ReadLine());
+            uus.virta();
+            Console.WriteLine("Anna kiukaan lämpötila >>");
+            uus.Lampotila = int.Parse(Console.ReadLine());
+            uus.lampotila();
+            Console.WriteLine("Kuinka monta kertaa kiukaalle heitetään vettä ? >>");
+            uus.Kosteus = int.Parse(Console.ReadLine());
+            uus.kosteus();*/
+
+            /*Lab02.Pesukone uus = new Lab02.Pesukone();
+
+            Console.WriteLine("Onko Pesukone päällä (1/0)? >>");
+            uus.Virta = int.Parse(Console.ReadLine());
+            uus.virta();
+            Console.WriteLine("Anna Pesu lämpötila >>");
+            uus.Lampotila = int.Parse(Console.ReadLine());
+            uus.lampotila();
+            Console.WriteLine("Kuinka Pitkän ohjelman haluat (min) ? >>");
+            uus.Aika = int.Parse(Console.ReadLine());
+            uus.aika();
+            Console.WriteLine("Kuinka paljon laitetaaan pesuainetta (ml) ? >>");
+            uus.Pesuaine = int.Parse(Console.ReadLine());
+            uus.pesuaine();
+            Console.WriteLine("Käynnistetäänkö pesuohjelma (1/0) ? >>");
+            uus.Ohjelma = int.Parse(Console.ReadLine());
+            uus.ohjelma();*/
+
+            Lab02.TV uus = new Lab02.TV();
+
+            Console.WriteLine("Laitetaanko TV päälle (1/0)? >>");
+            uus.Virta = int.Parse(Console.ReadLine());
+            uus.virta();
+            Console.WriteLine("Anna kanava >>");
+            uus.Kanava = int.Parse(Console.ReadLine());
+            uus.kanava();
+            Console.WriteLine("Muutetaanko äänenvoimakkuutta (-1 niin pienenee +1 niin suurenee) ? >>");
+            uus.Aanenvoimakkuus = int.Parse(Console.ReadLine());
+            uus.aanenvoimakkuus();
+
         }
     }
 }
