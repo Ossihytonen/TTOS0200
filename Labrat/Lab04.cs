2043372d7111e0b79187c1646621918e852849f8
Ossi <K9447@student.jamk.fi>
2017-09-22 21:10:10 +0300

    Added Lab04 (Teht 1-3) and Lab05 (Teht 1)

---
+++ Labrat/Lab04.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    public static class Lab04
+    {
+
+        public static bool OnkoLuku(string syote)
+        {
+            if (syote.All(char.IsDigit) || syote.ToLower().Contains(','))
+            {
+                return true;
+            }
+            if (syote.All(char.IsDigit) || syote.ToLower().Contains('+'))
+            {
+                return true;
+            }
+            if (syote.All(char.IsDigit) || syote.ToLower().Contains('-'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool OnkoPvm(string syote)
+        {
+            string[] format = new string[] { "dd.MM.yyyy", "dd.MM.yy" };
+            DateTime dateTime;
+
+            if (DateTime.TryParseExact(syote, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.NoCurrentDateDefault, out dateTime))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class Hissi
+    {
+        public int NykyinenKerros { get; set; }
+
+        public int Kerroksessa
+        {
+            get
+            {
+                return NykyinenKerros;
+            }
+            set
+            {
+                NykyinenKerros = value;
+            }
+        }
+    }
+    public class Vahvistin
+    {
+        public int Aanenvoimakkuus { get; set; }
+        public int Saato
+        {
+            set {
+                if (Aanenvoimakkuus > 100)
+                {
+                    Aanenvoimakkuus = 100;
+                }
+                if (Aanenvoimakkuus < 0)
+                {
+                    Aanenvoimakkuus = 0;
+                }
+            }
+            get
+            {
+                if (Aanenvoimakkuus > 100)
+                {
+                    return 100;
+                }
+                if (Aanenvoimakkuus < 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 5555;
+                }
+            }
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public int Salary { get; set; }
+        public string InfoE
+        {
+            get { return "Employee\n--|Name:" + Name + " |Profession: " + Profession + " |Salary: " + Salary; }
+        }
+    }
+    public class Boss : Employee
+    {
+        public string Car { get; set; }
+        public int Bonus { get; set; }
+        public string InfoB
+        {
+            get { return "Boss\n--|Name:" + Name + " |Profession: " + Profession + " |Salary: " + Salary+" |Car: "+ Car +" |Bonus: "+Bonus; }
+        }
+
+    }
+}
--- Labrat/Program.cs
+++ Labrat/Program.cs
@@ -52,7 +52,7 @@ namespace Labrat
             uus.Aanenvoimakkuus = int.Parse(Console.ReadLine());
             uus.AanenvoimakkuusAsennus();*/
 
-            Lab02.Vehicle uus = new Lab02.Vehicle();
+            /*Lab02.Vehicle uus = new Lab02.Vehicle();
 
             uus.Name = "Audi";
             uus.Speed = 300;
@@ -61,8 +61,97 @@ namespace Labrat
             Console.WriteLine(uus.SpeedConf());
             Console.WriteLine(uus.TyresConf());
             uus.PrintData();
-            Console.WriteLine(uus.ToSString());
+            Console.WriteLine(uus.ToSString());*/
+            /*
+            string syote2 = "kissa";
+            Console.WriteLine("kissa on luku {0}", Lab03.OnkoLuku(syote2));
+            string syote3 = "asd";
+            Console.WriteLine("asd on luku {0}", Lab03.OnkoLuku(syote3));
+            string syote4 = "3aw";
+            Console.WriteLine("3aw on luku {0}", Lab03.OnkoLuku(syote4));
+            string syote5 = "12-2";
+            Console.WriteLine("12-2 on luku {0}", Lab03.OnkoLuku(syote5));
+            string syote6 = "3";
+            Console.WriteLine("3 on luku {0}", Lab03.OnkoLuku(syote6));
+            string syote7 = "3,3";
+            Console.WriteLine("3,3 on luku {0}", Lab03.OnkoLuku(syote7));
+            Console.WriteLine("----------------------------------------------------");
+            string syote21 = "123";
+            Console.WriteLine("{0} on luku {1}", syote21, Lab03.OnkoLuku(syote21));
+            string syote22 = "-123";
+            Console.WriteLine("{0} on luku {1}", syote22, Lab03.OnkoLuku(syote22));
+            string syote23 = "+123";
+            Console.WriteLine("{0} on luku {1}", syote23, Lab03.OnkoLuku(syote23));
+            string syote24 = "8,123";
+            Console.WriteLine("{0} on luku {1}", syote24, Lab03.OnkoLuku(syote24));
+            string syote25 = "+9,123";
+            Console.WriteLine("{0} on luku {1}", syote25, Lab03.OnkoLuku(syote25));
+            string syote26 = "-9,123";
+            Console.WriteLine("{0} on luku {1}", syote26, Lab03.OnkoLuku(syote26));
+            string syote27 = ",";
+            Console.WriteLine("{0} on luku {1}", syote27, Lab03.OnkoLuku(syote27));
 
+            Console.WriteLine("----------------------------------------------------");
+            string syote11 = "11.11.1234";
+            Console.WriteLine("{0} on pvm {1}",syote11, Lab03.OnkoPvm(syote11));
+            string syote12 = "11,11,11";
+            Console.WriteLine("{0} on pvm {1}", syote11, Lab03.OnkoPvm(syote12));
+            string syote13 = "11,11,1111";
+            Console.WriteLine("{0} on pvm {1}", syote11, Lab03.OnkoPvm(syote13));
+            string syote14 = "11.11.11";*/
+
+            /*do
+            {
+                Hissi uus = new Hissi();
+                Console.WriteLine("Give a new floor number (1-5) >");
+                int kerros = int.Parse(Console.ReadLine());
+                if (kerros > 5 || kerros < 1)
+                {
+                    Console.WriteLine("Floor does not exist!");
+                }
+                else
+                {
+                    uus.NykyinenKerros = kerros;
+                    Console.WriteLine("Elevator is now in floor : {0}", uus.NykyinenKerros);
+                }
+            } while (true);*/
+            Vahvistin uus = new Vahvistin();
+            do
+            {
+                Console.WriteLine("Give a new volume value (0-100) >");
+                int aani = int.Parse(Console.ReadLine());
+                uus.Aanenvoimakkuus = aani;
+                if (aani > 100)
+                {
+                    uus.Saato = aani;
+                    Console.WriteLine("-> Too much volume -  Amplifier volume will be set to maximum :{0}", uus.Aanenvoimakkuus);
+                }
+                if (aani < 0)
+                {
+                    uus.Saato = aani;
+                    Console.WriteLine("-> Too low volume - Amplifier volume is set to minimum : {0}",uus.Aanenvoimakkuus);
+                }
+                else
+                {
+                    Console.WriteLine("-> Amplifier volume is set to : {0}", uus.Aanenvoimakkuus);
+                }
+            } while (true);
+            /*Employee nro1 = new Employee();
+            nro1.Name = "Kirsi Kurikka";
+            nro1.Profession = "Teatcher";
+            nro1.Salary = 1200;
+            Console.WriteLine(nro1.InfoE);
+            Boss nro2 = new Boss();
+            nro2.Name = "Pekka Varpunen";
+            nro2.Profession = "Leader";
+            nro2.Salary = 7000;
+            nro2.Car = "Saab";
+            nro2.Bonus = 4000;
+            Console.WriteLine(nro2.InfoB);
+            nro1.Name = "Kirsi Kurikka";
+            nro1.Profession = "Head Teatcher";
+            nro1.Salary = 4200;
+            Console.WriteLine(nro1.InfoE);*/
         }
     }
 }
