0de17ca3c9a4c01baadcf5a88782bac9eb3a9b3b
Ossi Hytönen <K9447@student.jamk.fi>
2017-10-25 10:49:41 +0300

    Lab8.1 teht

---
+++ Viikko43/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikko43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KÄYTTÄKÄÄ AINA TRY-CATCHIA
+            try
+            {
+                string nimi;
+                string filupath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filu = filupath + @"\temp.txt";
+                //kirjoitetaan käyttäjän antamat rivit tiedostoon
+                //Luodaan streamWriter tyyppinne olio johon kirjoitetaan
+                StreamWriter sw = new StreamWriter(filu);
+                do
+                {
+                    Console.Write("Anna nimi (Enter lopettaa): ");
+                    nimi = Console.ReadLine();
+                    if (nimi.Length > 0)
+                    {
+                        sw.WriteLine(nimi);
+                    }
+                } while (nimi.Length != 0);
+                sw.Close();
+                //avataan tiedosto uudestaan ja luetaan sen sisältö ja näytetään konsolissa
+                if (File.Exists(filu))
+                {
+                    string teksti = File.ReadAllText(filu);
+                    Console.WriteLine(teksti);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Tiedostoa ei löydy!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
