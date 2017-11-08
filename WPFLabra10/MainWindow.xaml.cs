522375c095b17bf8b6a16ec1d663c6c40687e1fe
Ossi Hytönen <K9447@student.jamk.fi>
2017-11-08 11:07:01 +0200

    Rekka/auto laskuri

---
+++ WPFLabra10/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLabra10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRekat_Click(object sender, RoutedEventArgs e)
+        {
+            String add = (Convert.ToInt32(txtRekat.Text) + 1).ToString();
+            txtRekat.Text = add;
+
+        }
+
+        private void btnAutot_Click(object sender, RoutedEventArgs e)
+        {
+            String add = (Convert.ToInt32(txtAutot.Text) + 1).ToString();
+            txtAutot.Text = add;
+        }
+    }
+}
