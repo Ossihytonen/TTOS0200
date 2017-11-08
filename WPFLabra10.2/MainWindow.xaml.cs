2737263093506687cf94b95fe7e7dbb39e29e755
Ossi <K9447@student.jamk.fi>
2017-11-08 20:42:56 +0200

    Pro Ikkunalaskuri 5000

--- WPFLabra10.2/MainWindow.xaml.cs
+++ WPFLabra10.2/MainWindow.xaml.cs
@@ -27,16 +27,53 @@ namespace WPFLabra10._2
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            double L, H, W, Ipa, Lpa, Kp;
-            L = double.Parse(ValueL.Text);
-            H = double.Parse(ValueH.Text);
-            W = double.Parse(ValueW.Text);
-            Lpa = ((H - 2 * W) * (L - 2 * W))/1000;
-            Kp = (H+H+L+L)/1000;
-            Ipa = (H * L)/1000;
-            txtLpa.Text = Lpa.ToString("0.00")+"m^2";
-            txtKp.Text = Kp.ToString("0.00")+"m^2";
-            txtIpa.Text = Ipa.ToString("0.00")+"m";
+            try
+            {
+                double L, H, W, Ipa, Lpa, Kp;
+                if (double.Parse(ValueL.Text) < 300 && double.Parse(ValueL.Text) > 0 )
+                {
+                    L = 300;
+                    ValueL.Text = "300";
+                }
+                if (double.Parse(ValueL.Text) > 3000)
+                {
+                    L = 3000;
+                    ValueL.Text = "3000";
+                }
+                else
+                {
+                    L = double.Parse(ValueL.Text);
+                }
+                if (double.Parse(ValueH.Text) < 300 && double.Parse(ValueH.Text) > 0)
+                {
+                    H = 300;
+                    ValueH.Text = "300";
+                }
+                if (double.Parse(ValueH.Text) > 3000)
+                {
+                    H = 3000;
+                    ValueH.Text = "3000";
+                }
+                else
+                {
+                    H = double.Parse(ValueL.Text);
+                }
+                W = double.Parse(ValueW.Text);
+                Lpa = ((H - 2 * W) * (L - 2 * W)) / 1000;
+                Kp = (H + H + L + L) / 1000;
+                Ipa = (H * L) / 1000;
+                Ikkuna.Width = L / 10;
+                Ikkuna.Height = H / 10;
+                txtLpa.Text = Lpa.ToString("0.00") + "m²";
+                txtKp.Text = Kp.ToString("0.00") + "m²";
+                txtIpa.Text = Ipa.ToString("0.00") + "m";
+            }
+            catch (Exception)
+            {
+                ValueH.Text = "0";
+                ValueL.Text="0";
+                ValueW.Text="45";
+            }
 
         }
     }
