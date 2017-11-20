66c9667c1201b3179c5f941247d51a91c6ad990a
Ossi <K9447@student.jamk.fi>
2017-11-20 11:42:23 +0200

    Lab11.2 done

---
+++ Labra11.2/BLPelaaja.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra11._2
+{
+    public class Pelaaja
+    {
+        public string Nimi { get; set; }
+        public string Pelipaikka { get; set; }
+        public string Kätisyys { get; set; }
+
+        public Pelaaja()
+        {
+        }
+        public Pelaaja(string nimi, string pelipaikka, string kätisyys)
+        {
+            Nimi = nimi;
+            Pelipaikka = pelipaikka;
+            Kätisyys = kätisyys;
+        }
+    }
+    public class PeliSeura
+    {
+        //public List<Pelaaja> pelaajat;
+        public static List<Pelaaja> HaePelaajat()
+        {
+            List<Pelaaja> pelaajat = new List<Pelaaja>();
+            PeliSeura JYP = new PeliSeura();
+            pelaajat.Add(new Pelaaja("Lauri Möttö", "Hyökkääjä", "Right"));
+            pelaajat.Add(new Pelaaja("Kalle Kankkunen", "Hyökkääjä", "Right"));
+            pelaajat.Add(new Pelaaja("J.P Hytönen", "Puolustaja", "Right"));
+            pelaajat.Add(new Pelaaja("Pasi Pakki", "Maalivahti", "Right"));
+            return pelaajat;
+
+        }
+    }
+}
---
+++ Labra11.2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra11._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Pelaaja> pelaajat2;
+        public MainWindow()
+        {
+            InitializeComponent();
+            pelaajat2 = PeliSeura.HaePelaajat();
+        }
+
+        private void btnHae_Click(object sender, RoutedEventArgs e)
+        {
+            pelaajat2 = PeliSeura.HaePelaajat();
+            dataGridPelaajat.ItemsSource = pelaajat2;
+        }
+    }
+}
