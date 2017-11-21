ac6d5f9770a79a1cfe36c2c8e3c0094ec16588b1
Ossi <K9447@student.jamk.fi>
2017-11-21 20:10:40 +0200

    3

---
+++ Labra11.3/BLLotto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra11._3
+{
+    public abstract class Lottery
+    {
+        public abstract string PelinNimi { get; }
+        public Random rng = new Random();
+        public List<int> ArvotutNumerot = new List<int>();
+        //mahdolliset lisänumerot
+        public List<int> Lisanumerot = new List<int>();
+        public abstract void Arvonta();
+
+        public string Tulostus()
+        {
+            ArvotutNumerot.Sort();
+            string s = "";
+            foreach (int i in ArvotutNumerot)
+            {
+                s += i.ToString() + ", ";
+            }
+            if (PelinNimi == "Eurojackpot")
+            {
+                s += "|| Lisänumerot: ";
+                foreach (int i in Lisanumerot)
+                {
+                    s += i.ToString() + ", ";
+                }
+            }
+            return s;
+        }
+    }
+    public class Lotto : Lottery
+    {
+        public override string PelinNimi { get { return "Lotto"; } }
+        public override void Arvonta()
+        {
+            ArvotutNumerot.Clear();
+            ArvotutNumerot = Enumerable.Range(1, 39 + 1).OrderBy(x => rng.Next()).Take(7).ToList();
+
+
+        }
+        public override string ToString()
+        {
+            return PelinNimi;
+        }
+    }
+    public class Vikinglotto : Lottery
+    {
+        public override string PelinNimi { get { return "Vikinglotto"; } }
+        public override void Arvonta()
+        {
+            ArvotutNumerot.Clear();
+            ArvotutNumerot = Enumerable.Range(1, 48 + 1).OrderBy(x => rng.Next()).Take(6).ToList();
+            
+        }
+        public override string ToString()
+        {
+            return PelinNimi;
+        }
+    }
+    public class Eurojackpot : Lottery
+    {
+        public override string PelinNimi { get { return "Eurojackpot"; } }
+        public override void Arvonta()
+        {
+            ArvotutNumerot.Clear();
+            Lisanumerot = Enumerable.Range(1, 11).OrderBy(x => rng.Next()).Take(2).ToList();
+            ArvotutNumerot = Enumerable.Range(1, 50 + 1).OrderBy(x => rng.Next()).Take(5).ToList();
+        }
+        public override string ToString()
+        {
+            return PelinNimi;
+        }
+    }
+}
--- Labra11.3/MainWindow.xaml.cs
+++ Labra11.3/MainWindow.xaml.cs
@@ -23,6 +23,40 @@ namespace Labra11._3
         public MainWindow()
         {
             InitializeComponent();
+            InitializeGames();
+        }
+        private void InitializeGames()
+        {
+            listGames.Items.Add(new Lotto());
+            listGames.Items.Add(new Vikinglotto());
+            listGames.Items.Add(new Eurojackpot());
+        }
+
+        private void btnDraw_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (listGames.SelectedIndex >= 0)
+                {
+                    Lottery peli = (Lottery)listGames.SelectedItem;
+                    Results.Text = "";
+                    int number = int.Parse(textDraws.Text);
+                    for (int x = 0; x < number; x++)
+                    {
+                        peli.Arvonta();
+                        Results.Text += String.Format("Rivi {0}: -- ", x + 1) + peli.Tulostus() + "\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Results.Text = ex.Message;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Results.Text = String.Empty;
         }
     }
 }
